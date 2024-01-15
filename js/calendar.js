8693ef5ba2dadfbd4a5e53427d6d90ae37a890c9
deraires <148113642+deraires@users.noreply.github.com>
2024-01-15 13:18:28 +0100

    Welcome cards for Upcomming events

--- js/calendar.js
+++
@@ -1,6 +0,0 @@
-var calendarMonth = document.getElementById("calendarMonth");
-var calendarDaysContent = document.getElementById("calendarDaysContent");
-var currentDate = new Date();
-var selectedDate = currentDate;
-
-
